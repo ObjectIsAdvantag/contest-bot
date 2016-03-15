bf1b6af23f05401d96167f4798e0e178aee0ef7d
ObjectIsAdvantag <steve.sfartz@gmail.com>
2016-03-15 15:20:26 +0100

    message details retrieved

--- spark/webhook.go
+++ spark/webhook.go
@@ -51,7 +51,8 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		log.Print("Expecting POST method as I am a Spark Webhook")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "{ message:'I am the ContestBot, expecting POST as new messages are typed into the Spark Room' }"
+		w.Header().Add("Content-type", "application/json")
+		fmt.Fprintf(w, `{ "message":"I am the ContestBot, expecting POST as new messages are typed into the Spark Room" }`)
 		return
 	}
 
@@ -59,16 +60,40 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
 	var event NewMessageEvent
 	if err := decoder.Decode(&event); err != nil {
-		log.Print("Could not parse json")
+		log.Print("Could not parse json to decode NewMessageEvent")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	log.Print("Processing event: %v", event)
 
 	// Retrieve message
-	
-	
-	
+	client, err := http.NewRequest("GET", "https://api.ciscospark.com/v1/messages/" + event.Data.ID, nil)
+	if err != nil {
+		log.Printf("Unexpected error while processing event: %s, retrieving message id: %s ", event.ID, event.Data.ID)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	token := "MmIzYTk0MWYtYzY2My00MjgzLThlZDQtOWU5ZmU1MzdiOTNiZTVhZWExOGEtM2Rh"
+	client.Header.Add("Content-type", "application/json")
+	client.Header.Add("Authorization", "Bearer " + token)
+
+	response, err := http.DefaultClient.Do(client)
+	if err != nil {
+		log.Printf("Unexpected error while retrieving contents for message id: %s ", event.ID, event.Data.ID)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Read message details
+	decoder = json.NewDecoder(response.Body)
+	var message SparkMessage
+	if err := decoder.Decode(&message); err != nil {
+		log.Print("Could not parse json to decode SparkMessage")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Print("Retrived message: %v", message)
+
 	w.WriteHeader(http.StatusOK)
 	return
 }
